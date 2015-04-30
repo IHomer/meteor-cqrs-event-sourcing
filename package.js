324d8e333a75e3865e24dad3f73bcaef5eb21c41
Wim Pool <wim.pool@ihomer.nl>
2015-04-30 16:14:38 +0200

    remove not used package mongo

--- package.js
+++ package.js
@@ -5,10 +5,9 @@ Package.describe({
 Package.on_use(function (api) {
     api.use('underscore', 'client');
     api.use('coffeescript', ['client', 'server']);
-
-    api.use('mongo', ['client', 'server']);
-   api.add_files('collections/events.coffee', ['client', 'server']);
-   api.add_files('cqrs/Command.coffee', ['client', 'server']);
-   api.add_files('cqrs/EventHandler.coffee', ['client', 'server']);
-   api.add_files('cqrs/methods.coffee', ['server']);
+    
+    api.add_files('collections/events.coffee', ['client', 'server']);
+    api.add_files('cqrs/Command.coffee', ['client', 'server']);
+    api.add_files('cqrs/EventHandler.coffee', ['client', 'server']);
+    api.add_files('cqrs/methods.coffee', ['server']);
 });
